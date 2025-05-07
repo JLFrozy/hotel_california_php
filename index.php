aab61e4d696f585dc99f744c4b943eefce1cda4c
Gabriel <103146230+JLFrozy@users.noreply.github.com>
2025-05-07 10:05:51 +0200

    Commentaire index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// Inclusion des fonctions d'authentification et de gestion des messages
 require_once 'auth/authFunctions.php';
 include_once 'assets/gestionMessage.php';
 ?>
@@ -7,11 +8,15 @@ include_once 'assets/gestionMessage.php';
 <head>
     <meta charset="UTF-8">
     <title>Gestion Hôtel California</title>
+    <!-- Inclusion de Bootstrap pour un design responsive -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Inclusion de Font Awesome pour les icônes -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <!-- Inclusion du fichier CSS personnalisé -->
     <link rel="stylesheet" href="./assets/style.css">
 </head>
 <body>
+    <!-- Barre de navigation Bootstrap -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.php">Hôtel California</a>
@@ -45,15 +50,18 @@ include_once 'assets/gestionMessage.php';
         </div>
     </nav>
 
+    <!-- Contenu principal -->
     <div class="container mt-4 text-center">
-        <?php displayMessage(); ?> <!- AJOUT DE CETTE LIGNE -->
-        <i class="fa-solid fa-cart-flatbed-suitcase fa-3x"></i> <h1 class="mb-4">Système de Gestion de l'Hôtel California</h1>
-
+        <?php displayMessage(); ?> <!-- Affichage des messages de succès/erreur -->
+        <i class="fa-solid fa-cart-flatbed-suitcase fa-3x"></i>
+        <h1 class="mb-4">Système de Gestion de l'Hôtel California</h1>
         <div class="text-center">
+            <!-- Image de l'hôtel -->
             <img src="./assets/HotelCalifornia.jpg" alt="Hôtel de nuit" class="img-fluid rounded hotel-image">
         </div>
     </div>
 
+    <!-- Inclusion du script Bootstrap pour les fonctionnalités interactives -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
